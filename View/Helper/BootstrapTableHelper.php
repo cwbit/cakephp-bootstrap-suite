2a2a1c44fef4a86b12ac70cbb1bcf6cd1a6b9124
Chris Brescia <chris@cwbit.com>
2014-07-22 16:36:02 -0400

    converted table helper to use multipart entity

--- View/Helper/BootstrapTableHelper.php
+++ View/Helper/BootstrapTableHelper.php
@@ -14,7 +14,7 @@ class BootstrapTableHelper extends BootstrapHelperEntityCollection{
 
 }
 
-class BootstrapTableEntity extends BootstrapHelperEntity{
+class BootstrapTableEntity extends BootstrapHelperMultipartEntity{
 	protected $_options = [
 		'tag'		=> 'table',
 		'baseClass' =>'table table-:hover table-:striped table-:bordered table-:condensed',
@@ -25,32 +25,25 @@ class BootstrapTableEntity extends BootstrapHelperEntity{
 		'content' => ':content',
 	];
 
-	public $Header = null;
-	public $Body = null;
-	public $Footer = null;
-
-	public function __construct(View $view, $settings = array()) {
-        parent::__construct($view, $settings);
-        $this->Body = new BootstrapTableBodyCollection($view, $settings);
-        $this->Header = new BootstrapTableHeaderCollection($view, $settings);
-        $this->Footer = new BootstrapTableFooterCollection($view, $settings);
-    }
-
-    public function __toString(){
-    	$result = [];
-    	$result[] = $this->Header->toString();
-    	$result[] = $this->Body->toString();
-    	$result[] = $this->Footer->toString();
-
-    	$options = $this->options();
-    	$options[$this->_contentToken] = implode(PHP_EOL, $result);
-
-    	$this->options($options);
-
-    	return parent::__toString();
-    }
+	protected $parts = [
+		'Header' => 'BootstrapTableHeaderCollection',
+		'Body' => 'BootstrapTableBodyCollection',
+		'Footer' => 'BootstrapTableFooterCollection',
+		];
 
 }
+
+class BootstrapTableRowCollection extends BootstrapHelperWrappedEntityCollection{
+	protected $_entityClass = 'BootstrapTableCellEntity';
+	protected $_options = [
+		'tag' => 'tr',
+	];
+}
+		class BootstrapTableCellEntity extends BootstrapHelperEntity{
+			protected $_options = [
+				'tag' => 'td',
+			];
+		}
 
 class BootstrapTableHeaderCollection extends BootstrapHelperWrappedEntityCollection{
 	protected $_entityClass = 'BootstrapTableHeaderRowCollection';
@@ -58,18 +51,21 @@ class BootstrapTableHeaderCollection extends BootstrapHelperWrappedEntityCollect
 		'tag' => 'thead',
 	];
 }
+		/* Table header row/cell are just specialized versions of normal */
+		class BootstrapTableHeaderRowCollection extends BootstrapTableRowCollection{
+			protected $_entityClass = 'BootstrapTableHeaderCellEntity';
+		}
+
+				class BootstrapTableHeaderCellEntity extends BootstrapTableCellEntity{
+					protected $_options = [
+						'tag' => 'th',
+					];
+				}
 
 class BootstrapTableBodyCollection extends BootstrapHelperWrappedEntityCollection{
 	protected $_entityClass = 'BootstrapTableRowCollection';
 	protected $_options = [
 		'tag' => 'tbody',
-	];
-}
-
-class BootstrapTableRowCollection extends BootstrapHelperWrappedEntityCollection{
-	protected $_entityClass = 'BootstrapTableCellEntity';
-	protected $_options = [
-		'tag' => 'tr',
 	];
 }
 
@@ -81,20 +77,9 @@ class BootstrapTableFooterCollection extends BootstrapHelperWrappedEntityCollect
 
 }
 
-class BootstrapTableCellEntity extends BootstrapHelperEntity{
-	protected $_options = [
-		'tag' => 'td',
-	];
-}
 
-/* Table header row/cell are just specialized versions of normal */
-class BootstrapTableHeaderRowCollection extends BootstrapTableRowCollection{
-	protected $_entityClass = 'BootstrapTableHeaderCellEntity';
-}
-class BootstrapTableHeaderCellEntity extends BootstrapTableCellEntity{
-	protected $_options = [
-		'tag' => 'th',
-	];
-}
+
+
+
 
 ?>
